52031dba8f6da271d849397c652474a35c1e63d4
ullas <ullaskunder3@gmail.com>
2022-01-08 13:10:29 +0530

    refactoring code

--- src/component/AddContact.js
+++ src/component/AddContact.js
@@ -1,48 +1,46 @@
 import React from "react";
-class AddContact extends React.Component{
-    state={
-        name:"",
-        email:"",
-        number:""
+class AddContact extends React.Component {
+    state = {
+        name: "",
+        email: "",
+        number: ""
     }
-    addform=(e)=>{
+    addform = (e) => {
         e.preventDefault();
-        if(this.state.name==="" || this.state.email==="" || this.state.number===""){
+        if (this.state.name === "" || this.state.email === "" || this.state.number === "") {
             alert("All Fields Needed");
             return;
         }
-    this.props.addConatcthandler(this.state);
-    this.setState({name:"",email:"",number:""});
+        this.props.addConatcthandler(this.state);
+        this.setState({ name: "", email: "", number: "" });
     };
-render(){
-    return(
+    render() {
+        return (
+            <div className="ui main">
+                <h2>Add Contat</h2>
 
+                <form className="ui form" onSubmit={ this.addform }>
+                    <div className="field">
+                        <label>Name</label>
+                        <input type="text" name="name" placeholder="Enter Name"
+                            value={ this.state.name } onChange={ (e) => this.setState({ name: e.target.value }) } />
+                    </div>
+                    <div className="field">
+                        <label>Email</label>
+                        <input type="text" name="email" placeholder="Enter Email"
+                            value={ this.state.email } onChange={ (e) => this.setState({ email: e.target.value }) } />
+                    </div>
+                    <div className="field">
+                        <label>Mobile No</label>
+                        <input type="text" name="number" placeholder="Enter Number"
+                            value={ this.state.number } onChange={ (e) => this.setState({ number: e.target.value }) } />
+                    </div>
+                    <button className="ui button blue" >Add</button>
+                </form>
 
-        <div className="ui main">
-<h2>Add Contat</h2>
-
-<form className="ui form" onSubmit={this.addform}>
-    <div className="field">
-        <label>Name</label>
-        <input type="text" name="name" placeholder="Enter Name"
-        value={this.state.name} onChange={(e)=>this.setState({name:e.target.value})}/>
-    </div>
-    <div className="field">
-        <label>Email</label>
-        <input type="text" name="email" placeholder="Enter Email"
-        value={this.state.email} onChange={(e)=>this.setState({email:e.target.value})}/>
-    </div>
-    <div className="field">
-        <label>Mobile No</label>
-        <input type="text" name="number" placeholder="Enter Number" 
-        value={this.state.number} onChange={(e)=>this.setState({number:e.target.value})}/>
-    </div>
-    <button className="ui button blue" >Add</button>
-</form>
-
-        </div>
-    )
-}
+            </div>
+        )
+    }
 
 }
 export default AddContact;
